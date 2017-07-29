3889afbfb06d3f224e0ead66435d758013748ec1
bradley sevison <bradleysev@gmail.com>
2017-07-29 12:24:18 -0600

    minor changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAvjXwHZGrkQs7Y9EfFlEDVqv63lqrcYp4",
+    authDomain: "rps-multiplayer-65f3e.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-65f3e.firebaseio.com",
+    projectId: "rps-multiplayer-65f3e",
+    storageBucket: "rps-multiplayer-65f3e.appspot.com",
+    messagingSenderId: "490371197400"
+  };
+  firebase.initializeApp(config);
